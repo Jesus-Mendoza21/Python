b78ff2384c3c3d69f943c143dbcc386143722760
Jesus-Mendoza21 <AL07082327@tecmilenio.mx>
2024-09-27 09:14:49 -0600

    Add files via upload

---
+++ Condicionale.py
@@ -0,0 +1,28 @@
+
+Numero = int(input("Proporciona un numero: "))
+if  Numero % 2 == 0:
+ print(f"El numero {Numero} es par")
+else:
+ print(f"El numero {Numero} es impar")
+
+
+#Ejercicio
+#Realizar un
+#Programa para saber si la persona cumple ciertas condiciones para acceder
+edad = int(input( ¿cual es tu edad?))
+acceso = input("Tienes acceso")
+ if(edad>= and acceso == "si")
+    print("puede acceder")
+else:
+    print("NO TIENES ACCESO")
+
+
+
+
+
+
+
+
+
+
+
